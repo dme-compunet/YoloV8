65554f8f31bf0b17b9c50610b25f791c3ff0da51
compunet <compunet.develope@gmail.com>
2023-06-21 18:51:32 +0300

    Change PlotImage to extension method

---
+++ Source/YoloV8/Plotting/PlottingExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Compunet.YoloV8.Data;
+
+namespace Compunet.YoloV8.Plotting;
+
+public static class PlottingExtensions
+{
+    public static Image PlotImage(this IPoseResult result, Image origin)
+    {
+        return PlottingUtilities.PlotImage(origin, result);
+    }
+
+    public static Image PlotImage(this IDetectionResult result, Image origin)
+    {
+        return PlottingUtilities.PlotImage(origin, result);
+    }
+}
