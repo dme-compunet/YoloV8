459fb7fef56aeb6585697bd15999585118e02de4
compunet <compunet.develope@gmail.com>
2023-06-30 13:40:01 +0300

    Add segmentation task support

---
+++ Source/YoloV8/Data/SegmentationResult.cs
@@ -0,0 +1,26 @@
+﻿using Compunet.YoloV8.Timing;
+
+namespace Compunet.YoloV8.Data;
+
+internal class SegmentationResult : YoloV8Result, ISegmentationResult
+{
+    public IReadOnlyList<ISegmentationBoundingBox> Boxes { get; }
+
+    public SegmentationResult(Size image, 
+                              SpeedResult speed,
+                              IReadOnlyList<ISegmentationBoundingBox> boxes) 
+        : base(image, speed)
+    {
+        Boxes = boxes;
+    }
+
+    public override string ToString()
+    {
+        var sort = Boxes.Select(x => x.Class)
+                        .GroupBy(x => x.Id)
+                        .OrderBy(x => x.Key)
+                        .Select(x => $"{x.Count()} {x.First().Name}");
+
+        return string.Join(", ", sort);
+    }
+}
