23d65142a8e20f68222348bba4581ee454d6f26f
compunet <compunet.develope@gmail.com>
2023-06-30 14:04:44 +0300

    Add segmentation demo

--- Source/YoloV8.Demo/Program.cs
+++ Source/YoloV8.Demo/Program.cs
@@ -8,42 +8,9 @@ var output = "./assets/output";
 if (Directory.Exists(output) == false)
     Directory.CreateDirectory(output);
 
-DetectDemo("./assets/input/bus.jpg", "./assets/models/yolov8s.onnx");
-
 PoseDemo("./assets/input/demo.jpg", "./assets/models/yolov8s-pose.onnx");
-
-void DetectDemo(string image, string model)
-{
-    Console.WriteLine();
-    Console.WriteLine("================ DETECTION DEMO ================");
-    Console.WriteLine();
-
-    Console.WriteLine("Loading model...");
-    using var predictor = new YoloV8(model);
-
-    Console.WriteLine("Detecting...");
-    var result = predictor.Detect(image);
-
-    Console.WriteLine();
-
-    Console.WriteLine($"Result: {result}");
-    Console.WriteLine($"Speed: {result.Speed}");
-
-    Console.WriteLine();
-
-    Console.WriteLine("Plotting and saving...");
-    using var origin = Image.Load(image);
-
-    using var ploted = result.PlotImage(origin);
-
-    var directory = Path.GetDirectoryName(image);
-    var filename = Path.GetFileNameWithoutExtension(image);
-    var extension = Path.GetExtension(image);
-
-    var pathToSave = Path.Combine(output, Path.GetFileName(image));
-
-    ploted.Save(pathToSave);
-}
+DetectDemo("./assets/input/bus.jpg", "./assets/models/yolov8s.onnx");
+SegmentDemo("./assets/input/demo.jpg", "./assets/models/yolov8s-seg.onnx");
 
 void PoseDemo(string image, string model)
 {
@@ -70,11 +37,68 @@ void PoseDemo(string image, string model)
 
     using var ploted = result.PlotImage(origin);
 
-    var directory = Path.GetDirectoryName(image);
-    var filename = Path.GetFileNameWithoutExtension(image);
-    var extension = Path.GetExtension(image);
+    var pathToSave = Path.Combine(output, Path.GetFileName(image));
+
+    ploted.Save(pathToSave);
+}
+
+void DetectDemo(string image, string model)
+{
+    Console.WriteLine();
+    Console.WriteLine("================ DETECTION DEMO ================");
+    Console.WriteLine();
+
+    Console.WriteLine("Loading model...");
+    using var predictor = new YoloV8(model);
+
+    Console.WriteLine("Detecting...");
+    var result = predictor.Detect(image);
+
+    Console.WriteLine();
+
+    Console.WriteLine($"Result: {result}");
+    Console.WriteLine($"Speed: {result.Speed}");
+
+    Console.WriteLine();
+
+    Console.WriteLine("Plotting and saving...");
+    using var origin = Image.Load(image);
+
+    using var ploted = result.PlotImage(origin);
 
     var pathToSave = Path.Combine(output, Path.GetFileName(image));
+
+    ploted.Save(pathToSave);
+}
+
+void SegmentDemo(string image, string model)
+{
+    Console.WriteLine();
+    Console.WriteLine("================ SEGMENTATION DEMO ================");
+    Console.WriteLine();
+
+    Console.WriteLine("Loading model...");
+    using var predictor = new YoloV8(model);
+
+    Console.WriteLine("Detecting...");
+    var result = predictor.Segment(image);
+
+    Console.WriteLine();
+
+    Console.WriteLine($"Result: {result}");
+    Console.WriteLine($"Speed: {result.Speed}");
+
+    Console.WriteLine();
+
+    Console.WriteLine("Plotting and saving...");
+    using var origin = Image.Load(image);
+
+    using var ploted = result.PlotImage(origin);
+
+    var filename = $"{Path.GetFileNameWithoutExtension(image)}_seg";
+    var extension = Path.GetExtension(image);
+
+    var pathToSave = Path.Combine(output, filename + extension);
 
     ploted.Save(pathToSave);
 }
