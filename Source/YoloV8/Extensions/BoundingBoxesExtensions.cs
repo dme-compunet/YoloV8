f65a72be1a1c38e329babc0bec59359f43ec4adb
compunet <compunet.develope@gmail.com>
2023-07-02 00:22:42 +0300

    Add 'Summary' extension method

---
+++ Source/YoloV8/Extensions/BoundingBoxesExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Compunet.YoloV8.Data;
+
+namespace Compunet.YoloV8.Extensions;
+
+public static class BoundingBoxesExtensions
+{
+    public static string Summary(this IEnumerable<IBoundingBox> boxes)
+    {
+        var sort = boxes.Select(x => x.Class)
+                        .GroupBy(x => x.Id)
+                        .OrderBy(x => x.Key)
+                        .Select(x => $"{x.Count()} {x.First().Name}");
+
+        return string.Join(", ", sort);
+    }
+}
