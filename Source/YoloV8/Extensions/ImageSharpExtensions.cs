71f050e323db40ca46daea972e7cf10eac2c07c3
compunet1 <compunet.develope@gmail.com>
2023-08-11 14:36:48 +0300

    Move the optimized way into ForEachPixelOptimized

--- Source/YoloV8/Extensions/ImageSharpExtensions.cs
+++ Source/YoloV8/Extensions/ImageSharpExtensions.cs
@@ -5,37 +5,47 @@ public static class ImageSharpExtensions
     public static void ForEachPixel<TPixel>(this Image<TPixel> image, Action<Point, TPixel> action)
         where TPixel : unmanaged, IPixel<TPixel>
     {
+        if (ForEachPixelOptimized(image, action))
+            return;
+
         var width = image.Width;
         var height = image.Height;
 
-        var flag = image.DangerousTryGetSinglePixelMemory(out Memory<TPixel> memory);
-
-        if (flag)
+        Parallel.For(0, width, x =>
         {
-            var totalPixels = width * height;
-            Parallel.For(0, totalPixels, index =>
+            for (int y = 0; y < height; y++)
             {
-                int x = index % width;
-                int y = index / width;
-
                 var point = new Point(x, y);
-                var pixel = memory.Span[index];
+                var pixel = image[x, y];
 
                 action(point, pixel);
-            });
-        }
-        else
-        {
-            Parallel.For(0, width, x =>
-            {
-                for (int y = 0; y < height; y++)
-                {
-                    var point = new Point(x, y);
-                    var pixel = image[x, y];
+            }
+        });
+    }
 
-                    action(point, pixel);
-                }
-            });
-        }
+    private static bool ForEachPixelOptimized<TPixel>(this Image<TPixel> image, Action<Point, TPixel> action)
+        where TPixel : unmanaged, IPixel<TPixel>
+    {
+        var succes = image.DangerousTryGetSinglePixelMemory(out Memory<TPixel> memory);
+
+        if (succes == false)
+            return false;
+
+        var width = image.Width;
+        var height = image.Height;
+        var totalPixels = width * height;
+
+        Parallel.For(0, totalPixels, index =>
+        {
+            int x = index % width;
+            int y = index / width;
+
+            var point = new Point(x, y);
+            var pixel = memory.Span[index];
+
+            action(point, pixel);
+        });
+
+        return true;
     }
 }
