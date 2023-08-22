8c33e24fefaf4874241f9a3accf7351bc9b87846
compunet <compunet.develope@gmail.com>
2023-08-22 22:17:11 +0300

    Add Where extension method

--- Source/YoloV8/Extensions/ReadOnlyListExtensions.cs
+++ Source/YoloV8/Extensions/ReadOnlyListExtensions.cs
@@ -1,34 +1,52 @@
-﻿using System;
-using System.Diagnostics;
-using System.Threading.Tasks;
-
-namespace Compunet.YoloV8.Extensions;
+﻿namespace Compunet.YoloV8.Extensions;
 
 public static class ReadOnlyListExtensions
 {
-    public static IReadOnlyList<TResult> Select<TSource, TResult>(this IReadOnlyList<TSource> values, Func<TSource, TResult> selector)
+    public static IReadOnlyList<TResult> Select<TSource, TResult>(this IReadOnlyList<TSource> source, Func<TSource, TResult> selector)
     {
-        var array = new TResult[values.Count];
+        var array = new TResult[source.Count];
 
-        for (int i = 0; i < values.Count; i++)
+        for (int i = 0; i < source.Count; i++)
         {
-            var value = values[i];
+            var value = source[i];
             array[i] = selector(value);
         }
 
         return array;
     }
 
-    public static IReadOnlyList<TResult> SelectParallely<TSource, TResult>(this IReadOnlyList<TSource> values, Func<TSource, TResult> selector)
+    public static IReadOnlyList<TResult> SelectParallely<TSource, TResult>(this IReadOnlyList<TSource> source, Func<TSource, TResult> selector)
     {
-        var array = new TResult[values.Count];
+        var array = new TResult[source.Count];
 
-        Parallel.For(0, values.Count, index =>
+        Parallel.For(0, source.Count, index =>
         {
-            var value = values[index];
+            var value = source[index];
             array[index] = selector(value);
         });
 
         return array;
+    }
+
+    public static IReadOnlyList<TSource> Where<TSource>(this IReadOnlyList<TSource> source, Func<TSource, bool> predicate)
+    {
+        var count = default(int);
+
+        for (int i = 0; i < source.Count; i++)
+        {
+            if (predicate(source[i]))
+                count++;
+        }
+
+        var index = default(int);
+        var result = new TSource[count];
+
+        for (int i = 0; i < source.Count; i++)
+        {
+            if (predicate(source[i]))
+                result[index++] = source[i];
+        }
+
+        return result;
     }
 }
