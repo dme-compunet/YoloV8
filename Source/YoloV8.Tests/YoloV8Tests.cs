9efdb3a40f3aa32409c1c72d8842a50ffbd3693c
compunet <compunet.develope@gmail.com>
2023-09-01 13:17:31 +0300

    Initial tests

---
+++ Source/YoloV8.Tests/YoloV8Tests.cs
@@ -0,0 +1,50 @@
+namespace YoloV8.Tests;
+
+public class YoloV8Tests
+{
+    [Theory]
+    [InlineData("bus.jpg", "person:4;bus:1")]
+    [InlineData("demo.jpg", "person:2;sports ball:1;baseball bat:1;baseball glove:1")]
+    public void DetectionTest(string image, string objects)
+    {
+        var predictor = Predictors.GetPredictor(YoloV8Task.Detect);
+
+        image = Path.Combine("./assets/input", image);
+
+        var result = predictor.Detect(image);
+
+        var list = new List<(string name, int count)>();
+
+        foreach (var item in objects.Split(';'))
+        {
+            var split = item.Split(":");
+
+            var name = split[0];
+            var count = int.Parse(split[1]);
+
+            list.Add((name, count));
+        }
+
+        Assert.Equal(result.Boxes.Count, list.Sum(x => x.count));
+
+        foreach (var (name, count) in list)
+        {
+            Assert.Equal(result.Boxes.Where(x => x.Class.Name == name).Count(), count);
+        }
+    }
+
+    [Theory]
+    [InlineData("pizza.jpg", "pizza")]
+    [InlineData("teddy.jpg", "teddy")]
+    [InlineData("toaster.jpg", "toaster")]
+    public void ClassificationTest(string image, string label)
+    {
+        var predictor = Predictors.GetPredictor(YoloV8Task.Classify);
+
+        image = Path.Combine("./assets/input", image);
+
+        var result = predictor.Classify(image);
+
+        Assert.Equal(result.Class.Name, label);
+    }
+}
