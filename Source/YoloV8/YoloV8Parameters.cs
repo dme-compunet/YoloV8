41bfff2a5cf5f393c38af30867b7346d0f5b3c0f
Compunet <compunet.develope@gmail.com>
2023-09-13 21:53:51 +0300

    Add ProcessWithOriginalAspectRatio property to YoloV8Parameters class

--- Source/YoloV8/YoloV8Parameters.cs
+++ Source/YoloV8/YoloV8Parameters.cs
@@ -8,9 +8,12 @@ public class YoloV8Parameters
 
     public float IoU { get; set; }
 
+    public bool ProcessWithOriginalAspectRatio { get; set; }
+
     public YoloV8Parameters()
     {
         Confidence = .3f;
         IoU = .45f;
+        ProcessWithOriginalAspectRatio = false;
     }
 }
