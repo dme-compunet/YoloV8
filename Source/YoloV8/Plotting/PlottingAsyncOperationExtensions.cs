3837e5881e59b5007e41b185261601c2c2ec97ef
Compunet <compunet.develope@gmail.com>
2023-09-23 23:18:27 +0300

    Add plotting async operation

---
+++ Source/YoloV8/Plotting/PlottingAsyncOperationExtensions.cs
@@ -0,0 +1,60 @@
+﻿namespace Compunet.YoloV8.Plotting;
+
+public static class PlottingAsyncOperationExtensions
+{
+    #region Pose
+
+    public static async Task<Image> PlotImageAsync(this IPoseResult result, Image originImage)
+    {
+        return await Task.Run(() => result.PlotImage(originImage));
+    }
+
+    public static async Task<Image> PlotImageAsync(this IPoseResult result, Image originImage, PosePlottingOptions options)
+    {
+        return await Task.Run(() => result.PlotImage(originImage, options));
+    }
+
+    #endregion
+
+    #region Detection
+
+    public static async Task<Image> PlotImageAsync(this IDetectionResult result, Image originImage)
+    {
+        return await Task.Run(() => result.PlotImage(originImage));
+    }
+
+    public static async Task<Image> PlotImageAsync(this IDetectionResult result, Image originImage, DetectionPlottingOptions options)
+    {
+        return await Task.Run(() => result.PlotImage(originImage, options));
+    }
+
+    #endregion
+
+    #region Segmentation
+
+    public static async Task<Image> PlotImageAsync(this ISegmentationResult result, Image originImage)
+    {
+        return await Task.Run(() => result.PlotImage(originImage));
+    }
+
+    public static async Task<Image> PlotImageAsync(this ISegmentationResult result, Image originImage, SegmentationPlottingOptions options)
+    {
+        return await Task.Run(() => result.PlotImage(originImage, options));
+    }
+
+    #endregion
+
+    #region Classification
+
+    public static async Task<Image> PlotImageAsync(this IClassificationResult result, Image originImage)
+    {
+        return await Task.Run(() => result.PlotImage(originImage));
+    }
+
+    public static async Task<Image> PlotImageAsync(this IClassificationResult result, Image originImage, ClassificationPlottingOptions options)
+    {
+        return await Task.Run(() => result.PlotImage(originImage, options));
+    }
+
+    #endregion
+}
