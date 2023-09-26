ab3de74b79457ab9670eff9efc1209395f14c89b
Compunet <compunet.develope@gmail.com>
2023-09-26 18:14:40 +0300

    Improve ForEachPixel extension method

--- Source/YoloV8/Extensions/ImageSharpExtensions.cs
+++ Source/YoloV8/Extensions/ImageSharpExtensions.cs
@@ -1,51 +1,40 @@
 ﻿namespace Compunet.YoloV8.Extensions;
 
-public static class ImageSharpExtensions
+internal static class ImageSharpExtensions
 {
-    public static void ForEachPixel<TPixel>(this Image<TPixel> image, Action<Point, TPixel> action)
+    public static void ForEachPixel<TPixel>(this Image<TPixel> image, Action<Point, TPixel> onPixel)
         where TPixel : unmanaged, IPixel<TPixel>
     {
-        if (ForEachPixelOptimized(image, action))
-            return;
-
         var width = image.Width;
         var height = image.Height;
 
-        Parallel.For(0, width, x =>
+        if (image.DangerousTryGetSinglePixelMemory(out var memory))
         {
-            for (int y = 0; y < height; y++)
+            Parallel.For(0, width * height, index =>
             {
+                int x = index % width;
+                int y = index / width;
+
                 var point = new Point(x, y);
-                var pixel = image[x, y];
+                var pixel = memory.Span[index];
 
-                action(point, pixel);
-            }
-        });
-    }
-
-    private static bool ForEachPixelOptimized<TPixel>(this Image<TPixel> image, Action<Point, TPixel> action)
-        where TPixel : unmanaged, IPixel<TPixel>
-    {
-        var succes = image.DangerousTryGetSinglePixelMemory(out Memory<TPixel> memory);
-
-        if (succes == false)
-            return false;
-
-        var width = image.Width;
-        var height = image.Height;
-        var totalPixels = width * height;
-
-        Parallel.For(0, totalPixels, index =>
+                onPixel(point, pixel);
+            });
+        }
+        else
         {
-            int x = index % width;
-            int y = index / width;
+            Parallel.For(0, image.Height, y =>
+            {
+                var row = image.DangerousGetPixelRowMemory(y).Span;
 
-            var point = new Point(x, y);
-            var pixel = memory.Span[index];
+                for (int x = 0; x < image.Width; x++)
+                {
+                    var point = new Point(x, y);
+                    var pixel = row[x];
 
-            action(point, pixel);
-        });
-
-        return true;
+                    onPixel(point, pixel);
+                }
+            });
+        }
     }
 }
