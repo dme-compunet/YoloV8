036525178bce464eb1893540495a61dec551f489
Compunet <compunet.develope@gmail.com>
2023-09-26 21:40:54 +0300

    Improve preprocess, try first get continuous memory block of the entire image data

--- Source/YoloV8/Utilities/PreprocessHelper.cs
+++ Source/YoloV8/Utilities/PreprocessHelper.cs
@@ -15,18 +15,39 @@ internal static class PreprocessHelper
         var xPadding = (modelSize.Width - image.Width) / 2;
         var yPadding = (modelSize.Height - image.Height) / 2;
 
-        Parallel.For(0, image.Height, y =>
-        {
-            var row = image.DangerousGetPixelRowMemory(y).Span;
+        var width = image.Width;
+        var height = image.Height;
 
-            for (int x = 0; x < image.Width; x++)
+        // Try get continuous memory block of the entire image data
+        if (image.DangerousTryGetSinglePixelMemory(out var memory))
+        {
+            Parallel.For(0, width * height, index =>
             {
-                var pixel = row[x];
+                int x = index % width;
+                int y = index / width;
+
+                var pixel = memory.Span[index];
 
                 target[batch, 0, y + yPadding, x + xPadding] = pixel.R / 255f;
                 target[batch, 1, y + yPadding, x + xPadding] = pixel.G / 255f;
                 target[batch, 2, y + yPadding, x + xPadding] = pixel.B / 255f;
-            }
-        });
+            });
+        }
+        else
+        {
+            Parallel.For(0, height, y =>
+            {
+                var row = image.DangerousGetPixelRowMemory(y).Span;
+
+                for (int x = 0; x < width; x++)
+                {
+                    var pixel = row[x];
+
+                    target[batch, 0, y + yPadding, x + xPadding] = pixel.R / 255f;
+                    target[batch, 1, y + yPadding, x + xPadding] = pixel.G / 255f;
+                    target[batch, 2, y + yPadding, x + xPadding] = pixel.B / 255f;
+                }
+            });
+        }
     }
 }
