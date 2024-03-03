e296913c881a2a019d127d2a36c4ef3d2b59465d
Compunet <compunet.develope@gmail.com>
2024-03-03 09:35:24 +0200

    Init YoloV8Builder

---
+++ Source/YoloV8/Builder/IYoloV8Builder.cs
@@ -0,0 +1,16 @@
+﻿namespace Compunet.YoloV8;
+
+public interface IYoloV8Builder
+{
+    public IYoloV8Builder UseMetadateDetect();
+
+    public IYoloV8Builder UseCuda() => UseCuda(0);
+
+    public IYoloV8Builder UseCuda(int deviceId);
+
+    public IYoloV8Builder UseDefaultConfiguration();
+
+    public IYoloV8Builder WithConfiguration(Action<YoloV8Configuration> configure);
+
+    public YoloV8Predictor Build();
+}
