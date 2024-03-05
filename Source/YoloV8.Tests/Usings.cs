1eee3474bdc2f22999f5f48658546256bf446f7c
Compunet <compunet.develope@gmail.com>
2024-03-05 17:46:49 +0200

    Revesed sotr, class diff and test unit

--- Source/YoloV8.Tests/Usings.cs
+++ Source/YoloV8.Tests/Usings.cs
@@ -1,5 +1,8 @@
 global using Compunet.YoloV8;
 global using Compunet.YoloV8.Metadata;
+global using Compunet.YoloV8.Parsers;
+global using Compunet.YoloV8.Utilities;
+global using SixLabors.ImageSharp;
 global using System.ComponentModel;
 global using Xunit;
 global using YoloV8Predictor = Compunet.YoloV8.YoloV8;
