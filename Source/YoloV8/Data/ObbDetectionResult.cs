e44a5ef1a2b7c77eceedd122044ed6d3d426bc0a
Compunet <compunet.develope@gmail.com>
2024-03-13 13:59:06 +0200

    Code rearranging

---
+++ Source/YoloV8/Data/ObbDetectionResult.cs
@@ -0,0 +1,8 @@
+﻿namespace Compunet.YoloV8.Data;
+
+public class ObbDetectionResult : YoloV8Result
+{
+    public required ObbBoundingBox[] Boxes { get; init; }
+
+    public override string ToString() => Boxes.Summary();
+}
