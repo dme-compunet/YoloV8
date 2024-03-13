340d16dff8ec18da3023b1b55342fb5058896353
Compunet <compunet.develope@gmail.com>
2024-03-13 13:59:06 +0200

    Code rearranging

---
+++ Source/YoloV8/Parsers/ObbDetectionOutputParser.cs
@@ -0,0 +1,26 @@
+﻿namespace Compunet.YoloV8;
+
+internal readonly struct ObbDetectionOutputParser(YoloV8Metadata metadata, YoloV8Configuration configuration)
+{
+    public ObbBoundingBox[] Parse(Tensor<float> output, Size originSize)
+    {
+        var boxes = new ObbIndexedBoundingBoxParser(metadata, configuration).Parse(output, originSize);
+
+        var result = new ObbBoundingBox[boxes.Length];
+
+        for (int i = 0; i < boxes.Length; i++)
+        {
+            var box = boxes[i];
+
+            result[i] = new ObbBoundingBox
+            {
+                Class = box.Class,
+                Bounds = box.Bounds,
+                Angle = box.Angle,
+                Confidence = box.Confidence,
+            };
+        }
+
+        return result;
+    }
+}
