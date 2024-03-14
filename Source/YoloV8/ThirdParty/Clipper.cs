b32469dbd7ede5ecab95f2828a3ca53031d5824c
Compunet <compunet.develope@gmail.com>
2024-03-14 10:06:40 +0200

    Disable nullable in ChipperLib

--- Source/YoloV8/ThirdParty/Clipper.cs
+++ Source/YoloV8/ThirdParty/Clipper.cs
@@ -38,6 +38,8 @@
 *                                                                              *
 *******************************************************************************/
 
+#nullable disable
+
 //use_int32: When enabled 32bit ints are used instead of 64bit ints. This
 //improve performance but coordinate values are limited to the range +/- 46340
 //#define use_int32
