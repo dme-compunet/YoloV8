1774e0eb02a70c653502b01a51eb14f6d880b8d2
Compunet <compunet.develope@gmail.com>
2024-06-27 12:55:27 +0300

    Make SkeletonConnection tuple implicit constructor

--- Source/YoloV8/Plotting/SkeletonConnection.cs
+++ Source/YoloV8/Plotting/SkeletonConnection.cs
@@ -5,4 +5,6 @@ public readonly struct SkeletonConnection(int first, int second)
     public int First { get; } = first;
 
     public int Second { get; } = second;
+
+    public static implicit operator SkeletonConnection(ValueTuple<int, int> tuple) => new(tuple.Item1, tuple.Item2);
 }
