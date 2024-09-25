3e10fac9d580bb2f7a1a0c13e7460cf9e7f1cc6b
dme-compunet <117437050+dme-compunet@users.noreply.github.com>
2024-09-25 14:24:08 +0300

    Add YoloMetadataTests

---
+++ Source/YoloV8.Tests/YoloMetadataTests.cs
@@ -0,0 +1,53 @@
+﻿namespace YoloV8.Tests;
+
+public class YoloMetadataTests
+{
+    [Fact]
+    public void PredictorTaskTest()
+    {
+        Assert.Equal(YoloTask.Pose, Predictors.Pose.Metadata.Task);
+        Assert.Equal(YoloTask.Detect, Predictors.Detection.Metadata.Task);
+        Assert.Equal(YoloTask.Segment, Predictors.Segmentation.Metadata.Task);
+        Assert.Equal(YoloTask.Classify, Predictors.Classification.Metadata.Task);
+    }
+
+    [Fact]
+    public void MetadataParsingTest()
+    {
+        var author = "Ultralytics";
+        var description = "Ultralytics YOLOv8n model trained on coco.yaml";
+        var version = "8.2.79";
+        var batch = 1;
+        var imgsz = new Size(640, 1024);
+        var task = YoloTask.Detect;
+        var names = new YoloName[]
+        {
+            new(0, "person"),
+            new(1, "bicycle"),
+        };
+
+        var dictionary = new Dictionary<string, string>()
+        {
+            { nameof(author), author },
+            { nameof(description), description },
+            { nameof(version), version },
+            { nameof(batch), $"{batch}" },
+            { nameof(imgsz), $"[{imgsz.Height}, {imgsz.Width}]" },
+            { nameof(task), $"{task.ToString().ToLower()}" },
+            { nameof(names), $"{{{names[0]}, {names[1]}}}" }
+        };
+
+        var metadata = new YoloMetadata(dictionary, YoloArchitecture.YoloV8);
+
+        Assert.Equal(author, metadata.Author);
+        Assert.Equal(description, metadata.Description);
+        Assert.Equal(version, metadata.Version);
+        Assert.Equal(batch, metadata.BatchSize);
+        Assert.Equal(imgsz, metadata.ImageSize);
+        Assert.Equal(task, metadata.Task);
+
+        Assert.Equal(names.Length, metadata.Names.Length);
+        Assert.Equal($"{names[0]}", $"{metadata.Names[0]}");
+        Assert.Equal($"{names[1]}", $"{metadata.Names[1]}");
+    }
+}
