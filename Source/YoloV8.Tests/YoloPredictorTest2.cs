510f35399fa33fc3959119e21b945fee451d476a
dme-compunet <117437050+dme-compunet@users.noreply.github.com>
2024-09-29 12:09:53 +0300

    Add OBB tests

--- Source/YoloV8.Tests/YoloPredictorTest2.cs
+++ Source/YoloV8.Tests/YoloPredictorTest2.cs
@@ -17,7 +17,10 @@ public partial class YoloPredictorTest2
         Assert.Throws<InvalidOperationException>(() => Predictors.Detection.Segment(path));
         Assert.Throws<InvalidOperationException>(() => Predictors.Detection.Classify(path));
 
-        // TODO: Add OBB
+        Assert.Throws<InvalidOperationException>(() => Predictors.ObbDetection.Pose(path));
+        Assert.Throws<InvalidOperationException>(() => Predictors.ObbDetection.Detect(path));
+        Assert.Throws<InvalidOperationException>(() => Predictors.ObbDetection.Segment(path));
+        Assert.Throws<InvalidOperationException>(() => Predictors.ObbDetection.Classify(path));
 
         Assert.Throws<InvalidOperationException>(() => Predictors.Segmentation.Pose(path));
         Assert.Throws<InvalidOperationException>(() => Predictors.Segmentation.Detect(path));
@@ -52,7 +55,30 @@ public partial class YoloPredictorTest2
         }
     }
 
+    [Theory]
+    [InlineData("./images/obb.jpg", "./TestAssets/obb.txt")]
+    public void ObbDetection(string imagePath, string jsonPath)
+    {
+        var jsonData = File.ReadAllText(jsonPath);
+        var expectedResult = JsonSerializer.Deserialize<ObbDetection[]>(jsonData) ?? throw new JsonException();
+        var actualResult = Predictors.ObbDetection.DetectObb(imagePath).ToArray();
+
+        Assert.Equal(expectedResult.Length, actualResult.Length);
+
+        for (var i = 0; i < actualResult.Length; i++)
+        {
+            var expected = expectedResult[i];
+            var actual = actualResult[i];
+
+            Assert.Equal($"{expected.Name}", $"{actual.Name}");
+            Assert.Equal(expected.Confidence, actual.Confidence);
+            Assert.Equal(expected.Bounds, actual.Bounds);
+            Assert.Equal(expected.Angle, actual.Angle);
+        }
+    }
+
     [JsonSerializable(typeof(Detection[]))]
+    [JsonSerializable(typeof(ObbDetection[]))]
     [JsonSourceGenerationOptions(WriteIndented = true)]
     partial class PredictionJsonContext : JsonSerializerContext { }
 }
