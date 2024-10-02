fe31789249c27ed7fef5d49ef0291c95af9514ea
dme-compunet <117437050+dme-compunet@users.noreply.github.com>
2024-10-02 15:59:10 +0300

    Initialize plotting DI

---
+++ Source/YoloV8/Contracts/Services/Plotting/IMaskDrawer.cs
@@ -0,0 +1,6 @@
+﻿namespace Compunet.YoloV8.Contracts.Services.Plotting;
+
+internal interface IMaskDrawer
+{
+    public void DrawMask(Segmentation prediction, PlottingContext context);
+}
---
+++ Source/YoloV8/Services/Plotting/SegmentationPlotter.cs
@@ -0,0 +1,16 @@
+﻿namespace Compunet.YoloV8.Services.Plotting;
+
+internal class SegmentationPlotter(IBoxDrawer boxDrawer,
+                                   INameDrawer nameDrawer,
+                                   IMaskDrawer maskDrawer) : IPlotter<Segmentation>
+{
+    public void Plot(YoloResult<Segmentation> result, PlottingContext context)
+    {
+        foreach (var box in result)
+        {
+            boxDrawer.DrawBox(box, context);
+            nameDrawer.DrawName(box, box.Bounds.Location, false, context);
+            maskDrawer.DrawMask(box, context);
+        }
+    }
+}
