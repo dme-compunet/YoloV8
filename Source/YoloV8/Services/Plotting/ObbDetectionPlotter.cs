04c0f46c7ab9e82f8c1f1d615917a3f000b09e9e
dme-compunet <117437050+dme-compunet@users.noreply.github.com>
2024-10-02 16:53:34 +0300

    Fix OBB plotting label position

--- Source/YoloV8/Services/Plotting/ObbDetectionPlotter.cs
+++ Source/YoloV8/Services/Plotting/ObbDetectionPlotter.cs
@@ -7,17 +7,20 @@ internal class ObbDetectionPlotter(IBoxDrawer boxPlotter,
     {
         foreach (var box in result)
         {
-            var points = GetBoxPoints(box);
+            var points = GetBoxPoints(box, out var namePosition);
 
             boxPlotter.DrawBox(box, points, context);
-            namePlotter.DrawName(box, points[0], false, context);
+            namePlotter.DrawName(box, namePosition, false, context);
         }
     }
 
-    private static PointF[] GetBoxPoints(ObbDetection detection)
+    private static PointF[] GetBoxPoints(ObbDetection detection, out PointF namePosition)
     {
         var points = detection.GetCornerPoints();
+        var result = points.Select(point => new PointF(point.X, point.Y));
 
-        return [.. points.Select(point => new PointF(point.X, point.Y))];
+        namePosition = result.MinBy(point => point.Y);
+
+        return [.. result];
     }
 }
